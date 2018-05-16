fcd6aaa3b1c0d8d7d32e3d18e544696f0bfcb48e
Jdlopezva <jdlopezva@unal.edu.co>
2018-05-16 17:28:44 -0400

    Rebuild

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var map;
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 14,
+  center: new google.maps.LatLng(40.7291,-73.9965),
+  mapTypeId: 'terrain'
+});
+
+// Create a <script> tag and set the USGS URL as the source.
+var script = document.createElement('script');
+// This example uses a local copy of the GeoJSON stored at
+// http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+script.src = 'https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson';
+document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+window.eqfeed_callback = function(results) {
+for (var i = 0; i < results.features.length; i++) {
+  var coords = results.features[i].geometry.coordinates;
+  var latLng = new google.maps.LatLng(coords[1],coords[0]);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+}
+}
